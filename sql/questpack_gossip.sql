0a265b4b95d6002387a2a2ccb8bce803b0662992
bewarellamas <97301098+bewarellamas@users.noreply.github.com>
2022-11-30 10:53:05 -0600

    upload again

---
+++ sql/questpack_gossip.sql
@@ -0,0 +1,12 @@
+DELETE FROM `gossip_menu` WHERE (`MenuID` = 62000) AND (`TextID` IN (197761));
+INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES
+(62000, 197761);
+
+DELETE FROM `gossip_menu` WHERE (`MenuID` = 62001) AND (`TextID` IN (197763));
+INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES
+(62001, 197762);
+
+DELETE FROM `gossip_menu` WHERE (`MenuID` = 62002) AND (`TextID` IN (197763));
+INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES
+(62002, 197763);
+
